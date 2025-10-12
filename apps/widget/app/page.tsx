edc2a55d7f74f177a7e2145f0f5a1d5890727786
Utkarsh Khajuria <157891400+UtkarsHMer05@users.noreply.github.com>
2025-10-12 23:54:30 +0530

    first commit

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,14 +1,11 @@
 import { Button } from "@workspace/ui/components/button"
-import { add } from "@workspace/math/add"
-import { Input } from "@workspace/ui/components/input"
+
 export default function Page() {
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
         <h1 className="text-2xl font-bold">Hello World</h1>
         <Button size="sm">Button</Button>
-        <p>{add(2, 2)}</p>
-        <input className="border border-gray-300 rounded px-2 py-1" placeholder="Input from shadcn/ui" />
       </div>
     </div>
   )
