c60e810d8a2e1a8b8fe0fc445201f5ae05a269a4
Utkarsh Khajuria <157891400+UtkarsHMer05@users.noreply.github.com>
2025-10-24 22:03:05 +0530

    done

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,15 +1,10 @@
-import { Button } from "@workspace/ui/components/button"
-import { add } from "@workspace/math/add"
-import { Input } from "@workspace/ui/components/input"
+import { useQuery } from "convex/react";
+import { api } from "@workspace/backend/convex/_generated/api";
 export default function Page() {
+  const users = useQuery(api.users.getMany);
   return (
     <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World</h1>
-        <Button size="sm">Button</Button>
-        <p>{add(2, 2)}</p>
-        <input className="border border-gray-300 rounded px-2 py-1" placeholder="Input from shadcn/ui" />
-      </div>
+
     </div>
   )
 }
