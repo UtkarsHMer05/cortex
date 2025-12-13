e9a469e9d569fce94fecea18dac4182c895d896a
Utkarsh Khajuria <157891400+UtkarsHMer05@users.noreply.github.com>
2025-12-14 02:30:47 +0530

    commit

---
+++ apps/web/app/(dashboard)/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+import { SignInButton, UserButton } from "@clerk/nextjs";
+import { AuthLoading, useMutation, useQuery } from "convex/react";
+import { api } from "@workspace/backend/_generated/api";
+import { Button } from "@workspace/ui/components/button";
+export default function Page() {
+  const users = useQuery(api.users.getMany);
+  const addUser = useMutation(api.users.add);
+  return (
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <Button onClick={() => addUser()}>Add</Button>
+      <p>Apps/web</p>
+      <UserButton />
+      <div className="max-w-sm w-full mx-auto">
+        {JSON.stringify(users, null, 2)}
+      </div>
+    </div>
+  )
+}
