bf1e876f79f546c8115b00e256f4bf80f3f1ac80
Utkarsh Khajuria <157891400+UtkarsHMer05@users.noreply.github.com>
2025-12-16 00:29:01 +0530

    commit

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,18 +1,14 @@
 "use client";
 
-import { useMutation, useQuery } from "convex/react";
+import { useMutation } from "convex/react";
 import { api } from "@workspace/backend/_generated/api";
 import { Button } from "@workspace/ui/components/button";
 export default function Page() {
-  const users = useQuery(api.users.getMany);
   const addUser = useMutation(api.users.add);
   return (
     <div className="flex flex-col items-center justify-center min-h-svh">
       <Button onClick={() => addUser()}>Add</Button>
       <p>Apps/widget</p>
-      <div className="max-w-sm w-full mx-auto">
-        {JSON.stringify(users, null, 2)}
-      </div>
     </div>
   )
 }
