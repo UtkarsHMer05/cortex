56fff1dd9bf541204b98df8f8452b9937528d7da
Utkarsh Khajuria <utkarshkhajuria7@gmail.com>
2026-02-13 01:03:12 +0530

    23 and 24:vapi-plugin-implemented

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -2,7 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google"
 
 import "@workspace/ui/globals.css"
 import { Providers } from "@/components/providers"
-
+import { Toaster } from "@workspace/ui/components/sonner"
 import { ClerkProvider } from "@clerk/nextjs"
 
 const fontSans = Geist({
@@ -27,6 +27,7 @@ export default function RootLayout({
       >
         <ClerkProvider>
           <Providers>
+            <Toaster />
             {children}
           </Providers>
         </ClerkProvider>
